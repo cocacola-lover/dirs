174d799dc55ff8babfdb1a971ff6e0e10d0e7983
Roman Ivashkin <pegas620570@gmail.com>
2024-01-30 19:25:55 +0300

    Add receiveInfo method to listener

--- pkg/listener/listener.go
+++ pkg/listener/listener.go
@@ -37,18 +37,46 @@ func askForInfo(w http.ResponseWriter, r *http.Request) {
 	}
 
 	ctx := r.Context()
-	taskCh := ctx.Value(taskChKey).(chan dtasks.BaseTask)
-	taskCh <- dtasks.NewAskInfoTask(&askInfoR)
+	taskCh := ctx.Value(taskChKey).(chan dtasks.ITask)
+	taskCh <- dtasks.NewOuterAskInfoTaskPointer(askInfoR)
 
 	fmt.Printf("got /ask request %s\n", string(body))
 }
 
-func Listen(taskCh chan dtasks.BaseTask) {
+func receiveInfo(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "POST" {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	body, readErr := io.ReadAll(r.Body)
+	if readErr != nil {
+		http.Error(w, "Error reading request body", http.StatusBadRequest)
+		return
+	}
+	defer r.Body.Close()
+
+	var sendInfoR drequests.SendInfoRequest
+	marshalErr := json.Unmarshal(body, &sendInfoR)
+	if marshalErr != nil {
+		http.Error(w, "Wrong json", http.StatusBadRequest)
+		return
+	}
+
+	ctx := r.Context()
+	taskCh := ctx.Value(taskChKey).(chan dtasks.ITask)
+	taskCh <- dtasks.NewSortInfoTaskPointer(sendInfoR)
+
+	fmt.Printf("got /ask request %s\n", string(body))
+}
+
+func Listen(taskCh chan dtasks.ITask) {
 	messageContext := context.Background()
 	messageContext = context.WithValue(messageContext, taskChKey, taskCh)
 
 	mux := http.NewServeMux()
 	mux.HandleFunc("/ask", askForInfo)
+	mux.HandleFunc("/send", receiveInfo)
 	serverOne := &http.Server{
 		Addr:    ":3333",
 		Handler: mux,
