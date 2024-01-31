2920273ac7a6c1df4e464ad67097176e39a0a4e3
Roman Ivashkin <pegas620570@gmail.com>
2024-01-31 20:15:02 +0300

    Make matchmaker into SortInfo and AskInfo task processor

--- pkg/matchmaker/matchmaker.go
+++ pkg/matchmaker/matchmaker.go
@@ -1,19 +1,41 @@
 package matchmaker
 
-import dtasks "dirs/pkg/tasks/baseTask"
+import dtasks "dirs/pkg/tasks"
 
 type Matchmaker struct {
-	// Waiting line for tasks
-	tasks []dtasks.ITask
+	// Search requests
+	requests map[string][]*dtasks.OuterAskInfoTask
 	// Array with stored info
 	store map[string]string
 }
 
-func (m Matchmaker) SearchStore(id string) (string, bool) {
-	val, ok := m.store[id]
-	return val, ok
+// Returns processedtrue if info found
+// else return false
+func (m Matchmaker) ProcessOuterAskInfoTask(task *dtasks.OuterAskInfoTask) bool {
+	val, ok := m.store[task.Search]
+
+	if ok {
+		task.Result = &val
+		return true
+	} else {
+		m.requests[task.Search] = append(m.requests[task.Search], task)
+		return false
+	}
 }
 
-func (m *Matchmaker) QueueUp(task dtasks.ITask) {
-	m.tasks = append(m.tasks, task)
+func (m Matchmaker) ProcessSortInfoTask(task *dtasks.SortInfoTask) []*dtasks.OuterAskInfoTask {
+	m.store[task.Search] = *task.Result
+
+	awaitingProcessing := m.requests[*task.Result]
+	m.requests[task.Search] = nil
+
+	for i := range awaitingProcessing {
+		awaitingProcessing[i].Result = task.Result
+	}
+
+	return awaitingProcessing
+}
+
+func NewMatchmaker() Matchmaker {
+	return Matchmaker{requests: make(map[string][]*dtasks.OuterAskInfoTask), store: make(map[string]string)}
 }
