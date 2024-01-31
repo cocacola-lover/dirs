47d6fc89604d2a66ea611f17c5bb65ea3848bb4e
Roman Ivashkin <pegas620570@gmail.com>
2024-01-31 20:15:36 +0300

    Add Process askinfoTask for broadcaster

--- pkg/broadcaster/broadcaster.go
+++ pkg/broadcaster/broadcaster.go
@@ -1,17 +1,44 @@
 package broadcaster
 
 import (
+	"bytes"
+	drequests "dirs/pkg/requests"
+	dtasks "dirs/pkg/tasks"
+	"encoding/json"
 	"fmt"
+	"io"
 	"net/http"
 )
 
-func SayHi(url string) error {
-	res, err := http.Get(url)
+func ProcessOuterAskInfoTask(task *dtasks.OuterAskInfoTask) error {
+	jsonBody, err := json.Marshal(&drequests.SendInfoRequest{Search: task.Search, Info: *task.Result})
 	if err != nil {
-		fmt.Printf("error making http request: %s\n", err)
+		fmt.Printf("client: could not marshal request: %s\n", err)
 		return err
 	}
-	fmt.Printf("client: got responce!\n")
+
+	bodyReader := bytes.NewReader(jsonBody)
+	req, err := http.NewRequest(http.MethodPost, task.From, bodyReader)
+
+	if err != nil {
+		fmt.Printf("client: could not create request: %s\n", err)
+		return err
+	}
+
+	res, err := http.DefaultClient.Do(req)
+	if err != nil {
+		fmt.Printf("client: error making http request: %s\n", err)
+		return err
+	}
+
+	fmt.Printf("client: got response!\n")
 	fmt.Printf("client: status code: %d\n", res.StatusCode)
+
+	resBody, err := io.ReadAll(res.Body)
+	if err != nil {
+		fmt.Printf("client: could not read response body: %s\n", err)
+		return err
+	}
+	fmt.Printf("client: response body: %s\n", resBody)
 	return nil
 }
