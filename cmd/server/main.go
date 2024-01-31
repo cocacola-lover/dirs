8e8e222d7934f25de05ba7dbd0e610ec1755920b
Roman Ivashkin <pegas620570@gmail.com>
2024-01-31 20:16:09 +0300

    Rewrite main to activate thinker

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,25 +1,11 @@
 package main
 
 import (
-	"dirs/pkg/listener"
-	dtasks "dirs/pkg/tasks"
-	"fmt"
+	"dirs/pkg/thinker"
 
 	_ "github.com/joho/godotenv/autoload"
 )
 
 func main() {
-	taskCh := make(chan dtasks.BaseTask)
-	go listener.Listen(taskCh)
-
-	for {
-		task, ok := <-taskCh
-
-		if !ok {
-			fmt.Println("Channel closed")
-			return
-		}
-
-		fmt.Printf("New task : %s\n", task.String())
-	}
+	thinker.InitThinker()
 }
